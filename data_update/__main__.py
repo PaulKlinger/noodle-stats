988a676d91226be6292ebdce988ebf4919d3645e
Paul Klinger <klinger.paul@gmail.com>
2022-10-24 22:49:13 +0100

    added & ran pre-commit hooks

--- data_update/__main__.py
+++ data_update/__main__.py
@@ -1,26 +1,25 @@
 import json
-from glob import glob
+import logging
 import os
 import re
-import logging
-
-import requests
-import pygsheets
-import numpy as np
-import pandas as pd
-import seaborn as sns
+from glob import glob
 
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-
+import numpy as np
+import pandas as pd
+import pygsheets
 import pysftp
+import requests
+import seaborn as sns
 
-from data_update.utils import GooglePhotos, put_r_portable
+from data_update.utils import GooglePhotos
+from data_update.utils import put_r_portable
 
 SECRETS = {
     "GDRIVE_SECRET": "./secrets/gdocs_service.json",
-    "GPHOTOS_SECRET":"./secrets/google_photos_credentials.json",
+    "GPHOTOS_SECRET": "./secrets/google_photos_credentials.json",
     "GPHOTOS_SECRET_STORAGE": "./secrets/photos_api_storage.json",
     "FTP_SECRET": "./secrets/ftp_secrets.json",
 }
@@ -37,7 +36,7 @@ def write_secrets_to_file():
 
 
 def get_tabular_data() -> dict[str, pd.DataFrame]:
-    #authorization
+    # authorization
     gc = pygsheets.authorize(service_file="./secrets/gdocs_service.json")
     noodle_sheet = gc.open("🐍 Noodle stats 🐍")
 
@@ -60,7 +59,7 @@ def get_tabular_data() -> dict[str, pd.DataFrame]:
     return {
         "feeding_df": feeding_df,
         "weight_df": weight_df,
-        "shed_ranges": shed_ranges
+        "shed_ranges": shed_ranges,
     }
 
 
@@ -72,76 +71,125 @@ def get_existing_photos() -> set[str]:
 def get_photos() -> set[str]:
     existing_photos = get_existing_photos()
 
-    p = GooglePhotos("./secrets/google_photos_credentials.json", "./secrets/photos_api_storage.json")
-    p.download_album("Public Noodle", "./web/data/photos/thumbnails/", 200, 200, exclude=existing_photos)
-    p.download_album("Public Noodle", "./web/data/photos/full_res/", 1500, 1500, exclude=existing_photos)
+    p = GooglePhotos(
+        "./secrets/google_photos_credentials.json", "./secrets/photos_api_storage.json"
+    )
+    p.download_album(
+        "Public Noodle",
+        "./web/data/photos/thumbnails/",
+        200,
+        200,
+        exclude=existing_photos,
+    )
+    p.download_album(
+        "Public Noodle",
+        "./web/data/photos/full_res/",
+        1500,
+        1500,
+        exclude=existing_photos,
+    )
 
-    return existing_photos | {os.path.basename(p) for p in glob("./web/data/photos/full_res/*")}
+    return existing_photos | {
+        os.path.basename(p) for p in glob("./web/data/photos/full_res/*")
+    }
 
 
-def create_json_data(shed_ranges: pd.DataFrame, feeding_df: pd.DataFrame, weight_df: pd.DataFrame, photo_fnames: set[str], output_path: str) -> None:
+def create_json_data(
+    shed_ranges: pd.DataFrame,
+    feeding_df: pd.DataFrame,
+    weight_df: pd.DataFrame,
+    photo_fnames: set[str],
+    output_path: str,
+) -> None:
     os.makedirs(os.path.split(output_path)[0], exist_ok=True)
 
     images = [
-        {"fname": fname,
-        "date": pd.to_datetime(match.group(1)).strftime("%Y-%m-%d")}
-        for fname in photo_fnames 
+        {"fname": fname, "date": pd.to_datetime(match.group(1)).strftime("%Y-%m-%d")}
+        for fname in photo_fnames
         if (match := re.search(r"(\d{8})_", fname)) is not None
     ]
     images.sort(key=lambda x: x["date"])
 
     data = {
-        "shedding": [[r.min.strftime("%Y-%m-%d"), r.max.strftime("%Y-%m-%d")] for r in shed_ranges.itertuples()],
+        "shedding": [
+            [r.min.strftime("%Y-%m-%d"), r.max.strftime("%Y-%m-%d")]
+            for r in shed_ranges.itertuples()
+        ],
         "feeding_accepted": {
-            "date": feeding_df.query("accepted")["date"].dt.strftime("%Y-%m-%d").to_list(),
+            "date": feeding_df.query("accepted")["date"]
+            .dt.strftime("%Y-%m-%d")
+            .to_list(),
             "food_weight": feeding_df.query("accepted")["food weight [g]"].to_list(),
         },
         "feeding_rejected": {
-            "date": feeding_df.query("~accepted")["date"].dt.strftime("%Y-%m-%d").to_list(),
+            "date": feeding_df.query("~accepted")["date"]
+            .dt.strftime("%Y-%m-%d")
+            .to_list(),
             "food_weight": feeding_df.query("~accepted")["food weight [g]"].to_list(),
         },
         "weight": {
             "date": weight_df["date"].dt.strftime("%Y-%m-%d").to_list(),
             "weight": weight_df["weight [g]"].to_list(),
         },
-        "images": images
+        "images": images,
     }
 
     with open(output_path, "w") as f:
         json.dump(data, f)
 
 
-def plot_static_graph(shed_ranges: pd.DataFrame, feeding_df: pd.DataFrame, weight_df: pd.DataFrame,) -> None:
+def plot_static_graph(
+    shed_ranges: pd.DataFrame,
+    feeding_df: pd.DataFrame,
+    weight_df: pd.DataFrame,
+) -> None:
     # start + end time of each shed and a nan y value in between to break the line
-    shed_plot_xs = sum(([r.min, r.max, r.max + pd.Timedelta(hours=1)] for r in shed_ranges.itertuples()), start=[])
-    shed_plot_ys = np.array(sum(([1, 1, np.nan] for _ in range(len(shed_ranges))), start=[]))
+    shed_plot_xs: list[pd.Timestamp] = sum(
+        (
+            [r.min, r.max, r.max + pd.Timedelta(hours=1)]
+            for r in shed_ranges.itertuples()
+        ),
+        start=[],
+    )
+    shed_plot_ys = np.array(
+        sum(([1, 1, np.nan] for _ in range(len(shed_ranges))), start=[])
+    )
 
-    blue, orange, green, red, *_ = sns.color_palette() # type: ignore
+    blue, orange, green, red, *_ = sns.color_palette()  # type: ignore
 
     fig, ax = plt.subplots(1, figsize=(20, 8))
-    plt.plot(weight_df["date"], weight_df["weight [g]"], marker="o", linewidth=3, color=orange)
-
+    plt.plot(
+        weight_df["date"],
+        weight_df["weight [g]"],
+        marker="o",
+        linewidth=3,
+        color=orange,
+    )
 
     f_accept = feeding_df.query("accepted")
     f_noaccept = feeding_df.query("~accepted")
     plt.bar(f_accept["date"], f_accept["food weight [g]"] * 10, color=green, width=2)
     plt.bar(f_noaccept["date"], f_noaccept["food weight [g]"] * 10, color=red, width=2)
 
-
     plt.xticks(rotation=30, ha="right")
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(10)) # type: ignore
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(10))  # type: ignore
     plt.grid(True, axis="y", linestyle=":")
     plt.ylabel("weight [g] | food weight [g / 10]")
-    plt.xticks(pd.date_range(weight_df["date"].min() - pd.Timedelta(weeks=4), weight_df["date"].max() + pd.Timedelta(weeks=4), freq="MS"))
+    plt.xticks(
+        pd.date_range(
+            weight_df["date"].min() - pd.Timedelta(weeks=4),
+            weight_df["date"].max() + pd.Timedelta(weeks=4),
+            freq="MS",
+        )
+    )
 
     plt.plot(shed_plot_xs, shed_plot_ys * 5, linewidth=5, marker="o")
-
 
     plt.legend(["weight", "sheds", "food accepted", "food not accepted"])
 
     plt.savefig("web/data/noodle_graph.svg")
 
-    matplotlib.rcParams['figure.dpi'] = 300
+    matplotlib.rcParams["figure.dpi"] = 300
     plt.savefig("web/data/noodle_graph.png", bbox_inches="tight")
     plt.close()
 
@@ -149,23 +197,31 @@ def plot_static_graph(shed_ranges: pd.DataFrame, feeding_df: pd.DataFrame, weigh
 def upload_to_server():
     with open("./secrets/ftp_secrets.json") as f:
         ftp_secrets = json.load(f)
-    
+
     cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None # type: ignore
+    cnopts.hostkeys = None  # type: ignore
 
     with pysftp.Connection(
-        ftp_secrets["NAMECHEAP_SERVER"], username=ftp_secrets["NAMECHEAP_USERNAME"],
-        password=ftp_secrets["NAMECHEAP_PASSWORD"], port=ftp_secrets["NAMECHEAP_PORT"],
-        cnopts=cnopts) as sftp:
-        
-        with sftp.cd('public_html'):
-            put_r_portable(sftp, "web", "noodle", skip_if_exists=(lambda p: p[-4:] in (".jpg", ".gif")))
+        ftp_secrets["NAMECHEAP_SERVER"],
+        username=ftp_secrets["NAMECHEAP_USERNAME"],
+        password=ftp_secrets["NAMECHEAP_PASSWORD"],
+        port=ftp_secrets["NAMECHEAP_PORT"],
+        cnopts=cnopts,
+    ) as sftp:
+
+        with sftp.cd("public_html"):
+            put_r_portable(
+                sftp,
+                "web",
+                "noodle",
+                skip_if_exists=(lambda p: p[-4:] in (".jpg", ".gif")),
+            )
 
 
 def main():
     logging.info("writing secrets to files")
     write_secrets_to_file()
-    
+
     logging.info("getting data from gsheets & preprocessing")
     tab_data = get_tabular_data()
 
@@ -173,7 +229,11 @@ def main():
     all_photo_fnames = get_photos()
 
     logging.info("creating json data")
-    create_json_data(**tab_data, photo_fnames=all_photo_fnames, output_path="./web/data/noodle_data.json")
+    create_json_data(
+        **tab_data,
+        photo_fnames=all_photo_fnames,
+        output_path="./web/data/noodle_data.json",
+    )
 
     logging.info("plotting graph")
     plot_static_graph(**tab_data)
--- data_update/utils.py
+++ data_update/utils.py
@@ -1,12 +1,16 @@
-from googleapiclient import discovery
-from httplib2 import Http
-from oauth2client import file, client, tools
-import requests
-import pysftp
-
 import os
 import shutil
-from typing import Any, Iterator, Callable
+from typing import Any
+from typing import Callable
+from typing import Iterator
+
+import pysftp
+import requests
+from googleapiclient import discovery
+from httplib2 import Http
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
 
 
 class GooglePhotos:
@@ -19,56 +23,75 @@ class GooglePhotos:
             self.credentials = tools.run_flow(
                 self.flow,
                 self.store,
-                flags=tools.argparser.parse_args([]) # type: ignore
-                ) 
-        self.photos = discovery.build("photoslibrary", "v1", http=self.credentials.authorize(Http()), static_discovery=False)
+                flags=tools.argparser.parse_args([]),  # type: ignore
+            )
+        self.photos = discovery.build(
+            "photoslibrary",
+            "v1",
+            http=self.credentials.authorize(Http()),
+            static_discovery=False,
+        )
 
     def get_album_id(self, album_title: str) -> str:
         albums = self.photos.albums()
         albums_meta = albums.list().execute()["albums"]
         return next(a for a in albums_meta if a["title"] == album_title)["id"]
 
-    def get_album_contents(self, album_id: str | None = None, album_title: str | None = None) -> Iterator[dict[str, Any]]:
+    def get_album_contents(
+        self, album_id: str | None = None, album_title: str | None = None
+    ) -> Iterator[dict[str, Any]]:
         if album_id is None:
             assert album_title is not None
             album_id = self.get_album_id(album_title)
-        
-        response = (self.photos
-                        .mediaItems()
-                        .search(body={"albumId": album_id})
-                        .execute())
+
+        response = self.photos.mediaItems().search(body={"albumId": album_id}).execute()
         yield from response["mediaItems"]
         while "nextPageToken" in response:
             response = (
-                self.photos
-                    .mediaItems()
-                    .search(body={
-                        "albumId": album_id,
-                        "pageToken": response["nextPageToken"]
-                        })
-                    .execute()
+                self.photos.mediaItems()
+                .search(
+                    body={"albumId": album_id, "pageToken": response["nextPageToken"]}
+                )
+                .execute()
             )
             yield from response["mediaItems"]
 
     @staticmethod
-    def download_item(base_url: str, target_width: int, target_height: int, output_path: str) -> None:
+    def download_item(
+        base_url: str, target_width: int, target_height: int, output_path: str
+    ) -> None:
         url = f"{base_url}=w{target_width}-h{target_height}"
         response = requests.get(url, stream=True)
-        
-        with open(output_path, 'wb') as out_file:
+
+        with open(output_path, "wb") as out_file:
             shutil.copyfileobj(response.raw, out_file)
 
-    def download_album(self, album_title: str, output_folder: str, target_width: int, target_height: int, exclude: set[str] | None = None) -> None:
+    def download_album(
+        self,
+        album_title: str,
+        output_folder: str,
+        target_width: int,
+        target_height: int,
+        exclude: set[str] | None = None,
+    ) -> None:
         os.makedirs(output_folder, exist_ok=True)
         for item in self.get_album_contents(album_title=album_title):
             if exclude is not None and item["filename"] in exclude:
                 continue
             print(".", end="")
             output_path = os.path.join(output_folder, item["filename"])
-            self.download_item(item["baseUrl"], target_width, target_height, output_path)
+            self.download_item(
+                item["baseUrl"], target_width, target_height, output_path
+            )
 
 
-def put_r_portable(sftp: pysftp.Connection, localdir: str, remotedir: str, preserve_mtime: bool = False, skip_if_exists: Callable[[str], bool] = lambda p: False) -> None:
+def put_r_portable(
+    sftp: pysftp.Connection,
+    localdir: str,
+    remotedir: str,
+    preserve_mtime: bool = False,
+    skip_if_exists: Callable[[str], bool] = lambda p: False,
+) -> None:
     # https://stackoverflow.com/a/58466685/7089433
     for entry in os.listdir(localdir):
         remotepath = remotedir + "/" + entry
@@ -76,7 +99,7 @@ def put_r_portable(sftp: pysftp.Connection, localdir: str, remotedir: str, prese
         if not os.path.isfile(localpath):
             try:
                 sftp.mkdir(remotepath)
-            except OSError:     
+            except OSError:
                 pass
             put_r_portable(sftp, localpath, remotepath, preserve_mtime, skip_if_exists)
         else:
